984bcd113ef8a4cc2533e3e05ab71446d1669768
Rafaelaltieris <raphaelaltieris@gmail.com>
2026-04-02 11:32:26 -0300

    Dto´s Usuario

---
+++ Models/DTOs/Usuario/UsuarioResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoList.Models.DTOs.Usuario
+{
+    public class UsuarioResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; } = "";
+        public string Email { get; set; } = "";
+        //public List<TarefaResponseDto> Tarefas { get; set; } = [];
+    }
+}
